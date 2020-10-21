5559a1c3f0280490e9bfe923ab4fb2867176ed68
Fkun <33350349+fkunde@users.noreply.github.com>
2020-10-21 16:18:44 +0200

    add feature

--- header.php
+++ header.php
@@ -8,8 +8,7 @@ session_start();
 <?php
 if (isset($_GET['tj']) AND $_GET['tj'] == 'logout') {
     session_start();
-    //开启session
-
+    //开启sessio
     unset($_SESSION['user_shell']);
     //session_destroy();  //注销session
     header("location:index.php");
@@ -27,7 +26,6 @@ if (isset($_GET['tj']) AND $_GET['tj'] == 'logout') {
     <link rel="stylesheet" href="css/bootstrap.min.css">
 	 <link rel="shortcut icon" href="https://fkun.tech/favicon.ico">
     <title>FKUN记账系统</title>
-
 </head>
 
 <body>
@@ -51,13 +49,11 @@ if (isset($_GET['tj']) AND $_GET['tj'] == 'logout') {
                         <a class="navbar-brand" href="add.php">记账</a>
                     </div>
                     <div class="collapse navbar-collapse" id="example-navbar-collapse">
-                        <ul class="nav navbar-nav">
-                             
+                        <ul class="nav navbar-nav">      
                             <li><a href="annual_stat.php">统计</a></li>
 							<li><a href="prediction.php">状态</a></li>
 							<li><a href="food.php">饮食</a></li>
 							<li><a href="plan.php">计划</a></li>
-
 							<li><a href="category.php">分类</a></li>
 							<li><a href="payway.php">方式</a></li>
                             <li><a href="search.php">导出</a></li>
@@ -67,7 +63,6 @@ if (isset($_GET['tj']) AND $_GET['tj'] == 'logout') {
                                 ?></a></li>
                             <li><a href="logout.php">登出</a></li>
                             <?php //index.php?tj=logout ?>
-
                         </ul>
                     </div>
                 </div>
