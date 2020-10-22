741a196822867fd4d2d885e4c7e152afeb0de38d
Fkun <33350349+fkunde@users.noreply.github.com>
2020-10-22 22:49:17 +0200

    Update food.php

---
+++ dev.php
@@ -0,0 +1,13 @@
+<form action ="index.php">
+
+<ul>
+  <li><input type ="checkbox" name ="chkFries" value ="11.00">Fries</li>
+  <li><input type ="checkbox" name ="chkSoda"  value ="12.85">Soda</li>
+  <li><input type ="checkbox" name ="chkShake" value ="1.30">Shake</li>
+  <li><input type ="checkbox" name ="chkKetchup" value =".05">Ketchup</li>
+</ul>
+<input type ="submit">
+</form>
+
+</body>
+</html>
