24e4a7a7111b8f296d2f77fcc6b6e46a219cd5e1
F_KUN <33350349+fkunde@users.noreply.github.com>
2022-04-17 17:46:15 +0200

    Update add.php

--- add.php
+++ add.php
@@ -104,7 +104,7 @@ if (isset($sql)) {
                 </select>
                 <font color="red"><a href="category.php" style="color:#7f7f7f;">管理类别</a></font>
                 <br />
-                <font color="red">此选项与收支类型，如果账目分类中找不到选项请参考<a href="https://bbs.fkun.tech/d/441">这里</a></font>
+                <font color="red">此选项与收支类型联动，如果找不到分类请看<a href="https://bbs.fkun.tech/d/441">这里</a></font>
                 <br /><br />
 
                 <font> 支付方式: </font><select name="payway" id="payid" style="height:26px;">
