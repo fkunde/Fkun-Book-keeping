4a7e668d8ee0d7f6e5f0627f10a0323b0942aea0
F_KUN <33350349+fkunde@users.noreply.github.com>
2022-07-13 22:35:06 +0200

    Update status.php

--- status.php
+++ status.php
@@ -348,6 +348,11 @@ include_once("header.php");
         </table>
 
         <?php
+                                    $firstsql = "SELECT * FROM " . $prename . "account where acuserid='$_SESSION[uid]' order by actime LIMIT 1 ";
+                                    $firstquery = mysqli_query($conn, $firstsql);
+                                    $first = mysqli_fetch_array($firstquery);
+                                    $firsttime = $first['actime'];
+                                    $firstdate = date('Y-m', $firsttime);
                                 $sqlmin = "select " . $prename . "month.mon as month, sum(case when " . $prename . "account.acamount is null then 0 else " . $prename . "account.acamount end) as sum from " . $prename . "month left join " . $prename . "account on date_format(FROM_UNIXTIME(" . $prename . "account.actime),'%Y-%m') = " . $prename . "month.mon where  " . $prename . "month.mon='" . $firstdate . "' group by month";
                                 $querymin = mysqli_query($conn, $sqlmin);
                                 $rowmin = mysqli_fetch_array($querymin);
